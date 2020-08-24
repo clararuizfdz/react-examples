a479df45bec904bba9e15533a4cc5aa7160ac535
Clara Ruiz Fernandez <clara.ruizfernandez@tui.be>
2020-08-24 11:34:03 +0200

    example 5  hooks: refactor custom Hooks in two hooks

--- 02-example/src/demo5.tsx
+++ 02-example/src/demo5.tsx
@@ -2,24 +2,32 @@ import React from "react";
 import { useDebounce } from "use-debounce";
 
 //Example custom hook "useUserCollection"
-
-const useUserCollection = () =>{
-
+const useDebounceFilter = () => {
     const [filter, setFilter] = React.useState("");
-    const [debounceFilter] =  useDebounce(filter, 500);
+    const [debounceFilter] = useDebounce(filter, 500);
+    return { filter, setFilter, debounceFilter };
+
+}
+
+const useUserCollection = () => {
+
     const [userCollection, setUserCollection] = React.useState([]);
 
-    React.useEffect(() =>{
+    const loadUsers = (filter) => {
         fetch(`https://swapi.dev/api/people?search=${filter}`)
-        .then(response => response.json())
-        .then(json => setUserCollection(json.results));
-    },[debounceFilter]);
-    return {filter, setFilter, userCollection};
+            .then(response => response.json())
+            .then(json => setUserCollection(json.results));
+    }
+    return { userCollection, loadUsers };
 }
 
 export const MyComponent5: React.FC = () => {
+    const { setFilter, filter, debounceFilter } = useDebounceFilter();
+    const { userCollection, loadUsers } = useUserCollection();
 
-    const {setFilter, filter, userCollection} = useUserCollection();
+    React.useEffect(() => {
+        loadUsers(debounceFilter)
+    }, [debounceFilter]);
     return (
         <div>
             <input value={filter} onChange={e => setFilter(e.target.value)} />
